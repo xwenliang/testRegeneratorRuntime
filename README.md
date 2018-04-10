28c1dcee283a1d9cd5263ae7d6f18b6231b4b6f9
xwenliang <wenliang.web@gmail.com>
2018-04-10 16:03:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# test regenerator-runtime
+
+### `npm install`
+
+### `node webpack.dev.js`
+
+### open 'http://localhost:3000'
+
+### `Uncaught TypeError: Cannot read property 'default' of undefined`
---
+++ index.js
@@ -0,0 +1,17 @@
+import regeneratorRuntime from 'regenerator-runtime';
+//test regenerator
+function sleep(time){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve();
+        }, time);
+    });
+};
+
+async function test(){
+    const time = 1000;
+    await sleep(time);
+    console.log(`I have waited for ${time} ms.`);
+};
+
+test();
